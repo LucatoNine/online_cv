312721af3dc82fb634541bf0a6f5990ca02955f3
Ynask <104215253+LucatoNine@users.noreply.github.com>
2024-02-06 13:26:26 +0100

    ADD CSS HTML JAVA

--- script.js
+++ script.js
@@ -29,20 +29,6 @@ window.addEventListener('scroll', function() {
     header.classList.toggle('sticky', window.scrollY > 0);
 });
 
-function openModal(imageSrc, title, description) {
-    const modal = document.getElementById('modal');
-    const modalImg = document.getElementById('modal-img');
-    const modalCaption = document.getElementById('modal-caption');
-    modal.style.display = 'block';
-    modalImg.src = imageSrc;
-    modalCaption.innerHTML = '<h2>' + title + '</h2><p>' + description + '</p>';
-}
-
-function closeModal() {
-    const modal = document.getElementById('modal');
-    modal.style.display = 'none';
-}
-
 document.querySelectorAll('.experience-more a').forEach(item => {
     item.addEventListener('click', event => {
         const experience = item.closest('.experience');
@@ -68,5 +54,30 @@ document.querySelectorAll('.formation-more a').forEach(item => {
             description.style.maxHeight = description.scrollHeight + "px";
             item.innerText = 'Réduire';
         } 
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const experienceSections = document.querySelectorAll('.experience-section');
+
+    experienceSections.forEach(section => {
+        section.addEventListener('click', () => {
+            // Masquer les éléments affichés précédemment
+            const visibleDetails = document.querySelector('.experience-section .details.show');
+            if (visibleDetails) {
+                visibleDetails.classList.remove('show');
+            }
+            const visibleImage = document.querySelector('.experience-section .image-container.show');
+            if (visibleImage) {
+                visibleImage.classList.remove('show');
+            }
+
+            // Afficher les éléments de cette section
+            const details = section.querySelector('.experience-details .details');
+            const image = section.querySelector('.image-container');
+            details.classList.add('show');
+            image.classList.add('show');
+        });
     });
 });
